bcd0afe8fa3106c47605345731ccb0dd8a12b0cd
mfahad <mfahadfadii@gmail.com>
2017-01-25 14:56:39 +0500

    changes

--- app/app.module.ts
+++ app/app.module.ts
@@ -31,6 +31,10 @@ import {DashboardComponent} from './dashboard.component';
 					  	path:"",
 					  	redirectTo: '/dashboard',
   						pathMatch: 'full'
+					  },
+					  {
+					  	path:'detail/:id',
+					  	component : HeroDetailComponent
 					  }
 
 					])
--- app/heroes.component.ts
+++ app/heroes.component.ts
@@ -14,7 +14,7 @@ import { HeroService } from './hero.service';
 				   	<span class="badge">{{hero.id}}</span> {{hero.name}}
 				  </li>
 				</ul>
-				<my-hero-detail [hero]="selectedHero"></my-hero-detail>
+				<my-hero-detail ></my-hero-detail>
  			`,
 
  	styles: [`
